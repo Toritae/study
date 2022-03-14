b7809bdde215e9298b6e7ca970d5a5c3d13f5efa
Toritae <60310842+Toritae@users.noreply.github.com>
2022-03-14 14:18:53 +0900

    Delete bj1439.py

--- bj1439.py
+++
@@ -1,9 +0,0 @@
-import sys
-a = list(sys.stdin.readline().strip())
-s = [a[0]]
-
-for i in range(1, len(a)):
-    if a[i-1] != a[i]:
-        s.append(a[i])
-
-print(min(s.count('0'), s.count('1')))
